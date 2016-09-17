a67c7b06208a57a28af35b281c5d7a060ecd18c2
Donncha O'Cearbhaill <donncha@donncha.is>
2016-09-18 01:02:32 +0200

    Fix bug finding socks port when a socks unix socket is also available

--- bwscanner/fetcher.py
+++ bwscanner/fetcher.py
@@ -14,7 +14,14 @@ def get_orport_endpoint(tor_state):
     proxy_endpoint = tor_state.protocol.get_conf("SocksPort")
 
     def extract_port_value(result):
-        port = result['SocksPort'].split()[0]
+        # Get the first SOCKS port number if any. SocksPort can be a single string or a list.
+        # Tor now also has support for unix domain SOCKS sockets so we need to be careful to just
+        # pick a SOCKS port.
+        if isinstance(result['SocksPort'], list):
+            port = next(port for port in result['SocksPort'] if port.isdigit())
+        else:
+            port = result['SocksPort']
+
         return int(port) if port != 'DEFAULT' else 9050
     proxy_endpoint.addCallback(extract_port_value)
     proxy_endpoint.addCallback(
