73bba063e47acdbdc67eacb913598db843e28a3c
David Stainton <dstainton415@gmail.com>
2016-10-17 00:59:58 +0000

    Rewrite partition detection using new lazy shuffle circuit generator
    also:
     - fix most of the unit tests
     - only log circuit build failures
     - cleanup CLI and add more parameters

--- bwscanner/partition_scan.py
+++ bwscanner/partition_scan.py
@@ -1,50 +1,65 @@
 """
-This scanner is used to make a connection to every possible pair of
-Tor relays to test if they can context to each other.
-
+This scanner is used detect partition Tor network partitions.
 Relays which cannot connect to each other are bad for Tor network health
-and it may indicate that an attack is being performed.
+and it may indicate that a partitioning attack is being performed.
 """
 import time
-
+import hashlib
+from twisted.internet.error import AlreadyCalled
 from twisted.internet import defer
-
-from bwscanner.circuit import FullyConnected
-from bwscanner.writer import ResultSink
 from txtorcon.circuit import build_timeout_circuit, CircuitBuildTimedOutError
+
+from bwscanner.writer import ResultSink
+from bwscanner.partition_shuffle import lazy2HopCircuitGenerator
 
 
 class ProbeAll2HopCircuits(object):
 
-    def __init__(self, state, clock, log_dir, stopped, partitions=1, this_partition=0):
+    def __init__(self, state,
+                 clock,
+                 log_dir,
+                 stopped,
+                 relays,
+                 shared_secret,
+                 partitions,
+                 this_partition,
+                 build_duration,
+                 circuit_timeout):
         """
         state: the txtorcon state object
         clock: this argument is normally the twisted global reactor object but
-        unit tests might set this to a clock object which can time travel for
-        speeding up tests.
+        unit tests might set this to a clock object which can time travel for faster testing.
         log_dir: the directory to write log files
         stopped: callable to call when done
-        partitions: the number of partitions to use for processing the
-        set of circuits
+        partitions: the number of partitions to use for processing the set of circuits
         this_partition: which partition of circuit we will process
+        build_duration: build a new circuit every specified duration
+        circuit_timeout: circuit build timeout duration
         """
         self.state = state
         self.clock = clock
         self.log_dir = log_dir
         self.stopped = stopped
+        self.relays = relays
+        self.shared_secret = shared_secret
         self.partitions = partitions
         self.this_partition = this_partition
+        self.circuit_life_duration = circuit_timeout
+        self.circuit_build_duration = build_duration
 
         self.lazy_tail = defer.succeed(None)
-        self.circuits = FullyConnected(self.state)
-        self.routers = self.state.routers
-        self.run_scan()
         self.tasks = []
+
+        consensus = ""
+        for relay in [str(relay.id_hex) for relay in relays]:
+            consensus += relay + ","
+        consensus_hash = hashlib.sha256(consensus).digest()
+        shared_secret_hash = hashlib.sha256(shared_secret).digest()
+        prng_seed = hashlib.pbkdf2_hmac('sha256', consensus_hash, shared_secret_hash, iterations=1)
+        self.circuits = lazy2HopCircuitGenerator(relays, this_partition, partitions, prng_seed)
 
         # XXX adjust me
         self.result_sink = ResultSink(log_dir, chunk_size=1000)
-        self.circuit_life_duration = 10
-        self.circuit_build_duration = .2
 
     def now(self):
         return 1000 * time.time()
@@ -53,10 +68,7 @@ class ProbeAll2HopCircuits(object):
         """
         Serialize a route.
         """
-        route_list = []
-        for router in route:
-            route_list.append("%r" % (router,))
-        return route_list
+        return "%s -> %s" % (route[0].id_hex, route[1].id_hex)
 
     def build_circuit(self, route):
         """
@@ -65,23 +77,13 @@ class ProbeAll2HopCircuits(object):
         """
         serialized_route = self.serialize_route(route)
 
-        def circuit_build_report(result):
-            time_end = self.now()
-            self.result_sink.send({"time_start": time_start,
-                                   "time_end": time_end,
-                                   "path": serialized_route,
-                                   "status": "ok",
-                                   "info": None})
-            return None
-
         def circuit_build_timeout(f):
             f.trap(CircuitBuildTimedOutError)
             time_end = self.now()
             self.result_sink.send({"time_start": time_start,
                                    "time_end": time_end,
                                    "path": serialized_route,
-                                   "status": "timeout",
-                                   "info": None})
+                                   "status": "timeout"})
             return None
 
         def circuit_build_failure(f):
@@ -89,26 +91,29 @@ class ProbeAll2HopCircuits(object):
             self.result_sink.send({"time_start": time_start,
                                    "time_end": time_end,
                                    "path": serialized_route,
-                                   "status": "failure",
-                                   "info": None})
+                                   "status": "failure"})
             return None
 
         time_start = self.now()
         d = build_timeout_circuit(self.state, self.clock, route, self.circuit_life_duration)
-        d.addCallback(circuit_build_report)
         d.addErrback(circuit_build_timeout)
         d.addErrback(circuit_build_failure)
         self.tasks.append(d)
 
-    def run_scan(self):
+    def start(self):
         def pop():
             try:
-                self.build_circuit(self.circuits.next())
+                route = self.circuits.next()
+                print self.serialize_route(route)
+                self.build_circuit(route)
             except StopIteration:
+                try:
+                    self.call_id.cancel()
+                except AlreadyCalled:
+                    pass
                 dl = defer.DeferredList(self.tasks)
                 dl.addCallback(lambda ign: self.result_sink.end_flush())
                 dl.addCallback(lambda ign: self.stopped())
-                self.call_id.cancel()
             else:
                 self.call_id = self.clock.callLater(self.circuit_build_duration, pop)
         self.clock.callLater(0, pop)
