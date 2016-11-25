932b931fd431a2952bbd89edfb96d5695f52c972
David Stainton <dstainton415@gmail.com>
2016-11-25 22:42:13 +0000

    Fix dependencies

--- setup.py
+++ setup.py
@@ -24,11 +24,6 @@ setup(name='bwscanner', # TODO: pick a better name
       packages=find_packages(),
       # data_files = [('path', ['filename'])]
       data_files=[],
-      extras_require={
-          "partition_detection": [
-              "pyblake2",
-          ],
-      },
       entry_points={
         "console_scripts": [
             'bwscan = bwscanner.scanner:cli',
