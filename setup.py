76d08be340430830fc5d59eb1c934927acb8045c
juga0 <juga@riseup.net>
2018-01-15 00:55:53 +0000

    Add classifiers, license, url

--- setup.py
+++ setup.py
@@ -3,20 +3,30 @@ from setuptools import setup, find_packages
 __version__ = '0.0.1'
 __author__ = 'aagbsn'
 __contact__ = 'aagbsn@torproject.org'
-__url__ = '' # TODO: publish this
-__license__ = ''
+__url__ = 'https://github.com/TheTorProject/bwscanner'  # TODO: publish this
+__license__ = 'GPL'
 __copyright__ = ''
 
-setup(name='bwscanner', # TODO: pick a better name
+setup(name='bwscanner',  # TODO: pick a better name
       version=__version__,
       description='Tor Bandwidth Scanner',
       long_description=__doc__,
       keywords=['python', 'twisted', 'txtorcon', 'tor', 'metrics'],
       install_requires=open('requirements.txt').readlines(),
-
-      # TODO: complete the classifiers
-      #classifiers = ['Framework :: Twisted', 'Programming Language :: Python']
-      classifiers=[],
+      classifiers=[
+        'Framework :: Twisted',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Internet',
+        'Topic :: Security',
+        'Topic :: Utilities'],
       author=__author__,
       author_email=__contact__,
       url=__url__,
