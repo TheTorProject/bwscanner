e82f33a8d637bd4ee47c2b568f674aff5e89cb06
juga0 <juga@riseup.net>
2018-03-21 16:23:06 +0000

    Code difference between develp_nowebagent and feature/minimal_code

--- test/test_circuit.py
+++ test/test_circuit.py
@@ -16,17 +16,6 @@ class TestCircuitGenerators(TorTestCase):
         assert all_exits == seen
         assert num_circuits == len(all_exits)
 
-    def test_two_hop(self):
-        all_r = set(self.routers)
-        num_circuits = 0
-        seen = set()
-        for circuit in TwoHop(self.tor_state):
-            assert len(circuit) == 2
-            assert 'exit' in circuit[-1].flags
-            num_circuits = num_circuits + 1
-            seen.add(circuit[0])
-        assert seen == all_r
-        assert num_circuits == len(all_r)
 
     def test_there_and_back_again(self):
         pass
